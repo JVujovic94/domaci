4e9eaa005bdbbcbce3b9f35e0f64d456a43e83f8
Jovana Vujovic <jovanavujovic94@gmail.com>
2022-01-10 23:14:04 +0100

    Smart air conditioning class & main class

---
+++ src/domaci_10_1_2022/D_Zadatak2.java
@@ -0,0 +1,33 @@
+package domaci_10_1_2022;
+
+import p10_1_2022_oop.SmartAirConditioning;
+
+public class D_Zadatak2 {
+
+	public static void main(String[] args) {
+//		Kreirati klasu SmartAirConditioning koja ima:
+//			atribut za marku klime (npr: Samsung, Galanz, Gree, …)
+//			atribut za izabranu temperaturu (minimum je 16, maksimum 35)
+//			atribut za mod (hladi/greje)
+//			metodu za stampu - stampa u formatu 
+		
+		SmartAirConditioning sac1 = new SmartAirConditioning ();
+		
+		sac1.marka = "Samsung";
+		sac1.temperatura = 18;
+		sac1.mod = "hladi";
+		sac1.stampaj();
+		
+		SmartAirConditioning sac2 = new SmartAirConditioning ();
+		
+		sac2.marka = "Vivax";
+		sac2.temperatura = 30;
+		sac2.mod = "greje";
+		sac2.stampaj();
+		
+		
+		
+
+	}
+
+}
---
+++ src/domaci_10_1_2022/SmartAirConditioning.java
@@ -0,0 +1,12 @@
+package domaci_10_1_2022;
+
+public class SmartAirConditioning {
+
+	String marka;
+	int temperatura;
+	String mod;
+	
+	public void stampaj () {
+		System.out.println(marka + " - temp: " + temperatura + " - mod: " + mod);
+	}
+}
