e8588248ccf2314644ba1a80877ca839ecd25864
Jovana Vujovic <jovanavujovic94@gmail.com>
2022-01-12 08:08:13 +0100

    Metoda dislike

--- src/domaci_11_1_2022/FacebookPost.java
+++ src/domaci_11_1_2022/FacebookPost.java
@@ -1,5 +1,51 @@
 package domaci_11_1_2022;
 
 public class FacebookPost {
+//	3..Kreirati klasu FacebookPost koja ima:
+//		   Od atributa:
+//		ime i prezime korisnika koji je objavio post
+//		ime i prezime korisnika na kom je profilu objavljen post (posto na fejsbuku mozete da postavite objavu na tudjem profilu)
+//		tekst objave
+//		broj lajkova
+//		broj deljenja
+	
+	String imePrezimeO;
+	String imePrezimeP;
+	String tekst;
+	int brojLajkova;
+	int brojDeljenja;
+	
+	public void like () {
+		this.brojLajkova = this.brojLajkova + 1;
+	}
+	
+
+	public void dislike () {
+		this.brojLajkova = this.brojLajkova - 1;
+		if (this.brojLajkova < 0) {
+			this.brojLajkova = 0;
+		}
+	}
+	
+	
+	
+	
+//		  Od metoda:
+//		like(), koja povecava broj lajkova za 1.
+//		dislike(), koja smanjuje broj lajkova za 1 (broj lajkova ne moze da bude manji od nule)
+//		share(), koja povecava broj deljenja za 1
+//		print(), koja stampa objavu u formatu:
+//		(ime i prezime osobe koja je objavila) >>> (ime i prezime na cijem profilu)
+//		(tekst objave)
+//		Likes (broj lajkova) | Shares (broj deljenja)
+//
+//		U glavnoj klasi napraviti minimum dva FacebookPost-a i nad svakim pozvati svaku funkciju.
+//
+//
+//		Primer stampanja:
+//		Milan Jovanovic >>> Pera Peric
+//		Ovo je tekst objave
+//		Likes 3 | Shares 1
+
 
 }
