6ac9c0c883697b9465aa1217f7f632d4cf24b914
Jovana Vujovic <jovanavujovic94@gmail.com>
2022-01-12 08:04:59 +0100

    Metoda like

---
+++ src/domaci_11_1_2022/D_Zadatak3.java
@@ -0,0 +1,10 @@
+package domaci_11_1_2022;
+
+public class D_Zadatak3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/domaci_11_1_2022/FacebookPost.java
@@ -0,0 +1,5 @@
+package domaci_11_1_2022;
+
+public class FacebookPost {
+
+}
