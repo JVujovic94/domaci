2e975a73471c41103928b1fa2050599e90fa38e3
Jovana Vujovic <jovanavujovic94@gmail.com>
2022-01-12 02:59:20 +0100

    Metoda stampaj

--- src/domaci_11_1_2022/Product.java
+++ src/domaci_11_1_2022/Product.java
@@ -11,32 +11,7 @@ public class Product {
 		System.out.print(this.naziv + ", cena:" + this.cena + " din, tezina:" + this.tezina + " g.");
 
 	}
-	
-	public void povecajCenu (int vrednostPovecanja) {
-		this.cena = this.cena + vrednostPovecanja;
-	}
-	
-	
-	public double vratiCenuSaPopustom (int popust) {
-		double cenaSaPopustom = this.cena - (this.cena*popust/100);
-		return cenaSaPopustom;
-	}
-	
-//	racunajPostarinu - funkcije vraća koliko bi bila postarina da se proizvod šalje preko kurirske sluzbe. 
-//	Funkcija je bez ulaznih parametara, a cena postarine se računa u zavisnosti od tezine:	
-//	za tezinu do 100g, postarina iznosi 200din
-//	za tezinu od 101g do 500g, postarina iznosi 400din
-//	za tezinu preko 500g, postarina iznosi 1000din
-	
-	public int racunajPostarinu () {
-		int cenaPostarine = 0;
-		if (this.tezina <= 100) {
-			cenaPostarine = 200;
-		} else if (this.tezina >= 101 && this.tezina <= 500) {
-			cenaPostarine = 400; 
-		} else {
-			cenaPostarine = 500;
+
+
 		}
-		return cenaPostarine;
-		}
-	}
+
