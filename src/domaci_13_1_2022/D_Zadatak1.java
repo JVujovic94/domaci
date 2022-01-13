f106cbc0c6c8f5c62a7ae3560568e41eb2989f3d
Jovana Vujovic <jovanavujovic94@gmail.com>
2022-01-13 23:38:29 +0100

    Getter, Setter, Constructor (FacebookPost)

--- src/domaci_13_1_2022/D_Zadatak1.java
+++ src/domaci_13_1_2022/D_Zadatak1.java
@@ -5,8 +5,37 @@ import domaci_11_1_2022.FacebookPost;
 public class D_Zadatak1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+//		1.Kreirati klasu FacebookPost koja ima:
+//			   Od atributa:
+//			ime i prezime korisnika koji je objavio post
+//			ime i prezime korisnika na kom je profilu objavljen post (posto na fejsbuku mozete da postavite objavu na tudjem profilu)
+//			tekst objave
+//			broj lajkova
+//			broj deljenja
+//			  Od gettera i settera:
+//			korisnik moze da procita bilo koji atribut 
+//			ali ne moze da menja broj lajkova i broj deljenja (nemamo settere za ove atribute)
+//			 Konstruktore:
+//				konstruktori koji su potrebni i logicni
+//			  Od metoda:
+//			like(), koja povecava broj lajkova za 1.
+//			dislike(), koja smanjuje broj lajkova za 1 (broj lajkova ne moze da bude manji od nule)
+//			share(), koja povecava broj deljenja za 1
+//			print(), koja stampa objavu u formatu:
+//			(ime i prezime osobe koja je objavila) >>> (ime i prezime na cijem profilu)
+//			(tekst objave)
+//			Likes (broj lajkova) | Shares (broj deljenja)
+//
+//			U glavnoj klasi napraviti minimum dva FacebookPost-a i nad svakim pozvati svaku funkciju.
+//
+//
+//			Primer stampanja:
+//			Milan Jovanovic >>> Pera Peric
+//			Ovo je tekst objave
+//			Likes 3 | Shares 1
+
+
+
 		FacebookPost fp1 = new FacebookPost ("Jovana Vujovic", "Ksenija Vujovic", "Happy Birthday! ♥");
 		
 		
