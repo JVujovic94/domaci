5d47bb75be72e4422135b0f72c3df14cde88d82e
Jovana Vujovic <jovanavujovic94@gmail.com>
2022-02-02 23:23:48 +0100

    31.1.2022. Zadatak 2.

---
+++ src/domaci_31_1_2022/zadatak1_vezbanje.java
@@ -0,0 +1,49 @@
+package domaci_31_1_2022;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class zadatak1_vezbanje {
+
+	public static void main(String[] args) throws InterruptedException {
+//		Napisati program koji:
+//		Ucitava stranicu https://s.bootsnipp.com/iframe/Dq2X
+//		Klikce na svaki iks da ugasi obavestenje i proverava da li se nakon klika element obrisao sa stranice 
+//		i ispisuje odgovarajuce poruke (OVO JE POTREBNO RESITI KORISCENJEM PETLJE)
+//		(ZA VEZBANJE)Probajte da resite da se elemementi brisu i odozgo
+
+
+	System.setProperty("webdriver.chrome.driver", 
+					"driver-lib/chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	
+	driver.navigate().to("https://s.bootsnipp.com/iframe/Dq2X");
+	Thread.sleep(3000);
+	driver.manage().window().maximize();
+	
+	
+	boolean obavestenjeUgaseno;
+	
+	for (int i = 5; i > 0; i--) {
+		driver.findElement(By.className("close")).click();
+	Thread.sleep(2000);
+	try {
+		 WebElement e = driver.findElement(By.className("alert alert-success-alt alert-dismissable"));
+		 obavestenjeUgaseno = false;
+	 } catch (Exception e2) {
+		 obavestenjeUgaseno = true;
+	 }
+	 
+	 if (obavestenjeUgaseno) {
+		 System.out.println("Obavestenje je ugaseno.");
+	 } else {
+		 System.out.println("Obavestenje nije ugaseno.");
+		 
+	 }
+		
+	}
+	}
+
+}
