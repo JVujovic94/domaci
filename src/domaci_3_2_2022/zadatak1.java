86863b3aafa39a4f3408f4a30da3769c981992d2
Jovana Vujovic <jovanavujovic94@gmail.com>
2022-02-07 08:16:25 +0100

    Domaci 3.2.2022. - Zadatak 1.

---
+++ src/domaci_3_2_2022/zadatak1.java
@@ -0,0 +1,36 @@
+package domaci_3_2_2022;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class zadatak1 {
+
+	public static void main(String[] args) {
+//		1.Zadatak 
+//		Napisati program koji:
+//		Ucitava stranicu https://www.google.com/
+//		Hvata body element sa stranicu i njemu setuje atribut 
+//		style na vrednost “background: nekaBoja”
+//		Skripta: arguments[0].style=”background: black”;
+//		(Za vezbanje) Setuje prosledjenu boju:
+//		Skripta: arguments[0].style=”background:” + arguments[1];
+//		Boje za testiranje - red, green, blue, …
+		
+
+		System.setProperty("webdriver.chrome.driver", 
+				"driver-lib/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		driver.navigate().to("https://www.google.rs");
+
+		WebElement e = driver.findElement(By.tagName("body"));
+
+		js.executeScript("arguments[0].style='background: black'", e);
+	}
+
+	}
